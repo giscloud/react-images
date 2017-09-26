266b1950564b1756fcb1883f9d5f54a1fce8163a
dcerjan <domagoj.cerjan@gmail.com>
2017-09-26 15:10:57 +0200

    in case an image is a document, only preview its extension

--- src/components/Thumbnail.js
+++ src/components/Thumbnail.js
@@ -5,29 +5,38 @@ import { css, StyleSheet } from 'aphrodite/no-important';
 import defaults from '../theme';
 import { deepMerge } from '../utils';
 
-function Thumbnail ({ index, src, thumbnail, active, onClick }, { theme }) {
+function Thumbnail ({ index, src, thumbnail, active, onClick, document }, { theme }) {
 	const url = thumbnail ? thumbnail : src;
 	const classes = StyleSheet.create(deepMerge(defaultStyles, theme));
 
 	return (
 		<div
-			className={css(classes.thumbnail, active && classes.thumbnail__active)}
+			className={css(classes.thumbnail, active && classes.thumbnail__active, document && classes.documentContainer)}
 			onClick={(e) => {
 				e.preventDefault();
 				e.stopPropagation();
 				onClick(index);
 			}}
-			style={{ backgroundImage: 'url("' + url + '")' }}
-		/>
+			style={document ? {} : { backgroundImage: 'url("' + url + '")' }}
+		>
+			{document && src.match(/\.(\w*)$/)
+				? <div className={css(classes.document)}>{src.match(/\.(\w*)$/)[0]}</div>
+				: null}
+		</div>
 	);
 }
 
 Thumbnail.propTypes = {
 	active: PropTypes.bool,
+	document: PropTypes.bool,
 	index: PropTypes.number,
 	onClick: PropTypes.func.isRequired,
 	src: PropTypes.string,
 	thumbnail: PropTypes.string,
+};
+
+Thumbnail.defaultProps = {
+	document: false,
 };
 
 Thumbnail.contextTypes = {
@@ -49,6 +58,15 @@ const defaultStyles = {
 	},
 	thumbnail__active: {
 		boxShadow: `inset 0 0 0 2px ${defaults.thumbnail.activeBorderColor}`,
+	},
+	documentContainer: {
+		backgroundColor: 'rgb(224, 224, 224)',
+		boxSizing: 'border-box',
+		position: 'relative',
+	},
+	document: {
+		color: 'rgb(122, 122, 122)',
+		lineHeight: `${defaults.thumbnail.size}px`,
 	},
 };
 
